cf688d64d73eb6150f6007be6707faef370c3bb2
AlexandruZaharia91 <zaharia.alexandru.catalin@gmail.com>
2024-02-15 20:11:11 +0200

    commit noi fisiere

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Haloo");
         Test test = new Test();
+        Test test3 = new Test();
+        test3.exemplu();
         test.exemplu();
     }
 }
